93a55527f75083c2cb9290fdf5ecfa5ad34287d8
Abhay <hackyabhay@gmail.com>
2024-05-18 18:33:48 +0530

    fixed issue
    ee

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {
-      fontFamily: {
-        'atma': ['Atma', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'],
-      },
-    },
-  },
-  plugins: [],
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    // Add other file paths as needed
+  ],
+  // Other Tailwind CSS configuration...
 }
